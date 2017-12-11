cb300e0e3015121166400a0de74dd9b1f9d373bb
Pradnya Kulkarni Telang <kpwpradnya@gmail.com>
2017-12-11 12:39:21 +0530

    Adding Basic Aurelia Project

---
+++ basic-aurelia-project/src/hello.js
@@ -0,0 +1,8 @@
+ export class Hello {
+      firstName = 'John';
+      lastName = 'Doe';
+
+      sayHello() {
+        alert(`Hello ${this.firstName} ${this.lastName}. Nice to meet you.`);
+      }
+    }
